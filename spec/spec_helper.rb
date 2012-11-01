8f984d9050eb69dcdc4b77cb48a002c1a3f9677a
unknown <Amit.Varshney@AMUSLT12713.GEO.CORP.HCL.IN>
2012-11-01 15:00:45 -0700

    finished static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,12 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
+Dir[Rails.root.join("spec/requests/*.rb")].each {|f| require f}
 RSpec.configure do |config|
   # ## Mock Framework
   #
